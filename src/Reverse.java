9a65238aff25c2894e0c31377b147202f3824c9c
RAKESH-RK <rakeshxyz@gmail.com>
2017-01-11 14:50:00 +0530

    Second Commit

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,8 +1,8 @@
 public class Reverse {
 	public static void main(String[] args) {
-		String name = "Rakesh Kumar";
+		//String name = "Rakesh Kumar";
 
-		StringBuilder reverseName = new StringBuilder(name).reverse();
+		StringBuilder reverseName = new StringBuilder(args[0]).reverse();
 		
 		System.out.println(reverseName);
 	}
